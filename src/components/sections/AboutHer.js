69b21bc330fcc6256e52c79e25baefbf5f5f1ffb
Cristina Villarroel <cvill141@fiu.edu>
2022-05-23 22:59:42 -0400

    Added more info in about page

--- src/components/sections/AboutHer.js
+++ src/components/sections/AboutHer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function AboutHer() {
     return (
-        <div className="columns w-row">
+        <div className="columns w-row reveal-from-left">
             <div className="leftcontent w-col w-col-6 w-col-stack">
             <div data-w-id="b84f5156-c6e2-fb1d-6606-98a08030a472" style={{opacity: 1}} className="image"></div>
             </div>
@@ -11,7 +11,19 @@ function AboutHer() {
                 <div className="name">Cristina Villarroel</div>
                 <h1 className="ceo"><strong className="bold-text">CEO</strong></h1>
                 <h1 className="tagline"><em className="bold-text">Software Engineer</em></h1>
-                <p className="bio">I'm originally from Valencia, Venezuela. I went to High School there and moved to the U.S when I was 18 years old. I graduated from Miami Dade College with an A.A and then finalized my Computer Science B.S at Florida International University in 2019. Upon graduation I got an offer to work for Visa as a Software Engineer where I worked on multiple innovative applications. </p>
+                <p className="bio">
+                    I'm from Valencia, Venezuela. I went to High School there and moved to the U.S when I was 18 years old.
+                    I went to Miami Dade College and then finalized my Computer Science studies at Florida International
+                    University with a B.S in 2019. Upon graduation I got an offer to work for FinTech company as a Software Engineer
+                    where I worked on multiple innovative applications.
+                </p>
+                <p className="bio">
+                    2020 was a really rough year for me and I took 2021 to correct a lot of things in my life that didn't feel right. My
+                    support system, hearing from others, and my strong will was what got me out of a really bad mental state, and in the
+                    process of finding myself I learned a lot of things that I wish someone would had told me, that's how
+                    <span className="text-color-primary"> HPS</span> was born. I wanted to create a platform for women to share their own
+                    personal stories as well as being able to guide young women throught life's aspects by providing tools and resources.
+                </p>
                 <div className="links w-row">
                 <div className="column w-col w-col-4">
                     <div className="text-block-2">Connect</div>
