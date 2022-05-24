30bf2c6af68115a1aae52042679132685833d109
Cristina Villarroel <cvill141@fiu.edu>
2022-05-24 01:08:12 -0400

    Added resources page

--- src/views/Money.js
+++ src/views/Money.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Finance from '../components/sections/Finance';
 import RecentPostMoney from "../components/sections/RecentPostMoney";
 import FinancialsGoals from "../components/sections/FinancialGoals";
+import Resources from "../components/sections/Resources";
 import FinanceApp from "../components/sections/FinanceApp";
 
 const Money = () => {
@@ -12,6 +13,7 @@ const Money = () => {
         <Finance />
         <RecentPostMoney />
         <FinancialsGoals />
+        {/*<Resources />*/}
         <FinanceApp />
       </>
   );
